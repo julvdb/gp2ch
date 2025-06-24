4d96f1ac27894d6a74526524b4eb849471259894
Jul Van den Broeck <jul.vdb@hotmail.com>
2025-06-24 22:48:40 +0200

    Added support for audio file in arguments

--- src/core.py
+++ src/core.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import argparse
 from pathlib import Path
 from zipfile import ZipFile
@@ -30,20 +32,22 @@ def extract_gp(gp_file: Path) -> None:
         zip_file.extractall(TMP_GP_DIR)
 
 
-def convert_audio_to_ogg(root: ET.Element) -> None:
+def extract_audio_filepath_from_gpif(root: ET.Element) -> Path | None:
     # Try to find the embedded audio file path
     audio_element = root.find("Assets/Asset/EmbeddedFilePath")
     if audio_element is None:
-        return
+        return None
     audio_path_text = audio_element.text
     if not audio_path_text:
-        return
+        return None
     audio_path = TMP_GP_DIR / audio_path_text
-    if not audio_path.exists():
-        return
+    if not audio_path.exists() or not audio_path.is_file():
+        return None
+    return audio_path
 
-    # Load the audio file
-    audio = AudioSegment.from_file(audio_path)  # type: AudioSegment
+def export_audio_to_ogg(filepath: Path) -> None:
+    # Load the audio file and add silence
+    audio = AudioSegment.from_file(filepath)  # type: AudioSegment
     countdown_silence = AudioSegment.silent(duration=COUNTDOWN_TIME * 1000)
     audio = countdown_silence + audio
 
@@ -52,7 +56,7 @@ def convert_audio_to_ogg(root: ET.Element) -> None:
     audio.export(TMP_OUT_DIR / SONG_FILENAME, format="ogg")
 
 
-def parse_gpif(gpif_file: Path) -> None:
+def parse_gpif(gpif_file: Path, audio_file: Optional[Path]=None) -> None:
     if not gpif_file.exists():
         raise FileNotFoundError(f"Error: {gpif_file} was not found.")
 
@@ -66,24 +70,44 @@ def parse_gpif(gpif_file: Path) -> None:
     chart.write_notes_chart_file(TMP_OUT_DIR / NOTES_FILENAME)
 
     # Convert the audio to OGG
-    convert_audio_to_ogg(root)
+    if audio_file is None or not audio_file.exists() or not audio_file.is_file():
+        audio_file = extract_audio_filepath_from_gpif(root)
+        if audio_file is None or not audio_file.exists() or not audio_file.is_file():
+            raise FileNotFoundError(
+                f"Error: No audio file found in {gpif_file} "
+                f"and no valid audio file specified."
+            )
+    export_audio_to_ogg(audio_file)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "input_file", type=str,
+        "-i",
+        "--input",
+        type=str,
+        required=True,
         help="Path to the Guitar Pro (.gp) file."
+    )
+    parser.add_argument(
+        "-a",
+        "--audio",
+        type=str,
+        required=False,
+        help="Path to the audio file."
     )
     args = parser.parse_args()
 
-    # Read the file path from the args
-    gp_file = Path(args.input_file)
+    # Read the GP file path from the args
+    gp_file = Path(args.input)
     extract_gp(gp_file)
+
+    # Read the audio file path from the args
+    audio_file = Path(args.audio) if args.audio else None
 
     # Parse the GPIF file
     gpif_file = TMP_GP_DIR / GPIF_PATH
-    parse_gpif(gpif_file)
+    parse_gpif(gpif_file, audio_file=audio_file)
 
 
 if __name__ == "__main__":
